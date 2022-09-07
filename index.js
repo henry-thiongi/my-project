05784f4bbfdf5c89668189ca9b8e4bbbd6a59372
henry-thiongi <boblilhot@gmail.com>
2022-09-07 22:36:00 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+
+// create variable and store the method and API key
+const options = {
+	method: 'GET',
+	headers: {
+	'X-RapidAPI-Key': '21fefbc796msh7d28334ae39e921p1ff81ejsn2d30ed627a80',
+	'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+	}
+	};
